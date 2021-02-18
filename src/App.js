60e39edbad82e9e45e7b97cf18e13498630886b2
ghost-lolade <40849064+ghost-lolade@users.noreply.github.com>
2021-02-18 14:38:36 +0000

    Built the markdown using react-markdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown';
+
 
 function App() {
+  const [markdown, setMarkdown] = useState('# Ghost markdown preview')
+
   return (
-    <main></main>
+    <main>
+      <section className='markdown'>
+        <textarea 
+        className='input' 
+        value={markdown} 
+        onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
   );
 }
 
